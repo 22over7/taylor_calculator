98f249bf5c101cc3c471b2105df0726eb05a1796
22over7 <lorenzsommer1@gmail.com>
2020-07-21 22:21:39 +0200

    aaaaa

---
+++ taylor_calculator.py
@@ -0,0 +1,40 @@
+import math
+
+def input_interpreter(function_input_simple_additive):
+    function_input.split("+")
+
+import re
+import math
+def derivative_calculator_ln(input_function_ln, derivative_order):
+
+    #matcher = re.compile(".*1/.+") #regex expression to allow for pattern matching of (factor)*1/x^(power)
+
+
+
+    #template for ln derivatives of order > 1
+    #ln_template = "1/{ln_divisor}"
+
+    #calculate factor, negative for odd values and vice versa
+    if derivative_order%2 == 0:
+        factor_ln = math.factorial(derivative_order-1) * (-1)
+    else:
+        factor_ln = math.factorial(derivative_order-1)
+
+    if derivative_order == 0:
+        #derivative order 1
+        return "ln(x)"
+
+    if derivative_order == 1:
+        #derivative order 1
+        return "1/x"
+
+
+    #derivative order > 1
+    if derivative_order > 1:
+
+        if derivative_order%2 == 0: #check for even magnitude
+            return str(factor_ln) + "*" + "1/" + "x^" + str(derivative_order)
+
+
+        if derivative_order%2 == 1: #check for odd magnitude
+            return str(factor_ln) + "*" + "1/" + "x^" + str(derivative_order)
